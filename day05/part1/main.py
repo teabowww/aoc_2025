be4cbede6e56bab7263afd821b8e1cb55cb6f10f
teabow <thibault@fernandez-guggisberg.name>
2025-12-05 17:46:05 +0100

    day 5, part 1 indentation

--- day05/part1/main.py
+++ day05/part1/main.py
@@ -13,13 +13,13 @@ with open(input_file) as file:
 	input_string = file.read()
 	input_parts = input_string.split("\n\n")
 
-	range_strings = input_parts[0]
-	ranges = [tuple(map(int, r.split("-"))) for r in range_strings.split("\n")]
+range_strings = input_parts[0]
+ranges = [tuple(map(int, r.split("-"))) for r in range_strings.split("\n")]
 
-	ids = map(int, input_parts[1].split("\n"))
+ids = map(int, input_parts[1].split("\n"))
 
-	for id_num in ids:
-		if in_ranges(id_num, ranges):
-			fresh_count += 1
+for id_num in ids:
+	if in_ranges(id_num, ranges):
+		fresh_count += 1
 
 print(fresh_count)
